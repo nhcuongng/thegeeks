cee6133029d954f7f64ebea0fe6758edadd36e3f
A Geek <cuong.nguyenhuu1997@gmail.com>
2018-01-05 02:10:45 +0700

    fixed image

--- image.py
+++
@@ -1,7 +0,0 @@
-from mongoengine import *
-# import mlab
-class Image(Document):
-    image = StringField()
-# mlab.connect()
-# new_image = Image('http://farm7.static.flickr.com/6215/6286018298_b6af252e8c_o.jpg')
-# new_image.save()
